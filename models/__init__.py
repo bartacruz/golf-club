7b47f6dc8ac0bbddd5c0aa1f37665946e81331d0
Julio Santa Cruz <bartacruz@gmail.com>
2026-04-02 01:04:50 -0300

    Small adjustments before split

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,14 @@
-from . import golf_card, golf_field, golf_hole, golf_tournament, res_config_settings, res_partner, golf_tournament_mode, golf_tournament_mode_strokes, golf_tournament_mode_gross_strokes, golf_tournament_mode_choice, website
+from . import (
+    golf_card,
+    golf_field,
+    golf_hole,
+    golf_tournament,
+    golf_tournament_mode,
+    golf_tournament_mode_strokes,
+    golf_tournament_mode_gross_strokes,
+    golf_tournament_mode_choice, 
+    product_template,
+    res_config_settings,
+    res_partner,
+    website,
+)
