e87d77b7fc2e07dd4cc60b222b234b117b268842
Julio Santa Cruz <bartacruz@gmail.com>
2026-04-02 19:25:54 -0300

    [FIX] pos_golf_club: clean up

--- pos_golf_club/models/golf_card.py
+++ pos_golf_club/models/golf_card.py
@@ -6,7 +6,12 @@ class GolfCard(models.Model):
     
     pos_order_line_id = fields.Many2one('pos.order.line', string='POS Order line', readonly=True)
     pos_order_id = fields.Many2one('pos.order', related="pos_order_line_id.order_id")
-    
+    @api.model
+
+    def _load_pos_data_fields(self, config_id):
+        print("_load_pos_data_fields de Golf Card",config_id)
+        return ['id', 'name', 'player_id', 'state', 'pos_order_line_id']
+
     def action_view_pos_order(self):
         action = self.env['ir.actions.act_window']._for_xml_id('point_of_sale.action_pos_pos_form')
         #action["views"]= [[self.env.ref("point_of_sale.view_pos_pos_form").id, "form"]],
--- pos_golf_club/models/pos_order.py
+++ pos_golf_club/models/pos_order.py
@@ -5,13 +5,13 @@ from odoo import models, fields, api
 class PosOrder(models.Model):
     _inherit = 'pos.order'
 
-    golf_card_ids = fields.Many2many('golf.card', string='Golf Card', readonly=True, copy=False,compute='_compute_golf_cards')
+    golf_card_ids = fields.Many2many('golf.card', string='Golf Card',compute='_compute_golf_cards', store=True)
 
     @api.depends('lines.golf_card_id')
     def _compute_golf_cards(self):
         for order in self:
             order.golf_card_ids = order.lines.mapped('golf_card_id')
-            
+    
     def _process_saved_order(self, draft):
         print("_process_saved_order self:",self,self.state,draft)
         ret = super()._process_saved_order(draft)
@@ -27,5 +27,4 @@ class PosOrder(models.Model):
             drafts.write({'state':'active'})
             drafts.message_post(body=f"Golf Card activated from POS order {record.name}")
             record.golf_card_ids.write({'account_move_id': record.account_move.id})
-        return ret               
-    
+        return ret
